fb4b909fd2c5f79c240611a8e4bd7080b72cfd52
sofilarrea <sofialejandra.larrea@gmail.com>
2023-04-26 20:44:22 -0300

    view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,24 @@
 class RestaurantsController < ApplicationController
-
   def index
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    
+  end
+
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
